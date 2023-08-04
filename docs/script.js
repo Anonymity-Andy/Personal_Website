d40842955c5ebdb979c99f691137cd3c66e15827
Anonymity-Andy <60261462+Anonymity-Andy@users.noreply.github.com>
2023-08-04 22:56:20 +0800

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,14 @@
+// // 获取半透明区域的DOM元素
+// const contentElement = document.querySelector('.content');
+
+// // 给半透明区域添加点击事件监听器
+// contentElement.addEventListener('click', () => {
+//   alert('这里是半透明区域！');
+// });
+
+// 给GitHub图标添加点击事件监听器
+// githubIcon.addEventListener('click', () => {
+//   window.open('https://github.com/anonymity-andy', '_blank');
+// });
+
+// 继续
